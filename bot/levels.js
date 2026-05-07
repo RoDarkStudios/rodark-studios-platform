09e5976234ba9928b64652019a080d3b5107725e
Myron Buist <myron@rodarkstudios.com>
2026-05-07 21:17:51 +0100

    Simplify level unlock message

--- bot/levels.js
+++ bot/levels.js
@@ -247,6 +247,7 @@ async function sendLevelUpAnnouncement(message, control, nextLevel, addedRoles) 
         ? ' You can now attach files and embed links.'
         : '';
     const roleNote = highestAddedRole
+        && nextLevel !== levelSystem.attachmentUnlockLevel
         ? ` You earned **${highestAddedRole.name}**.`
         : '';
 
